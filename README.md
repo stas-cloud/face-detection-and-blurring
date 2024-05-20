e1f6fa3c3e21fc523eb92584d19836dcaee09312
stas-cloud <stanislav.f.web@gmail.com>
2024-05-20 02:05:45 -0500

    Add finished project

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # face-detection-and-blurring
+
 Face detection and blurring the face/faces using python, opencv, and mediapipe. Three imput modes: image, video, web-cam. The script outputs image, video, or web-cam stream with blurred faces respectively.
+
+
+# how to install
+
+$ pip install -r requirements.txt
+
+
+# how to use
+
+$ python main.py --mode --filePath
+
+mode: image, vidoe, webcam
+
+filePath: path to your image or video
+
+
+# screenshot examples
+
+image-example
+![alt text](image-2.png)
+
+vidoe-example
+![alt text](image-1.png)
+
+web-cam-example
+![alt text](image.png)
+
+# note
+
+./data folder contains test video and test image
+after running the script with --video or --image flags, script will make ./output folder
+and will same output file there
--- main.py
+++ main.py
@@ -1,24 +1,16 @@
 import cv2
 import mediapipe as mp
 import os
+import argparse
 
 
 output_dir = './output'
 
-if not os.path.exists(output_dir):
-    os.makedirs(output_dir)
 
-# read image
-img_path = './data/testImg.png'
+def process_img(img, face_detection):
 
-img = cv2.imread(img_path)
+    H, W, _ = img.shape
 
-H, W, _ = img.shape
-
-# detect faces
-mp_face_detection = mp.solutions.face_detection
-
-with mp_face_detection.FaceDetection(model_selection=0, min_detection_confidence=0.5) as face_detection:
     img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     out = face_detection.process(img_rgb)
     
@@ -35,8 +27,68 @@ with mp_face_detection.FaceDetection(model_selection=0, min_detection_confidence
             h = int(h * H)
 
     # blur faces
-    img[y1:y1 + h, x1:x1 + w, :] = cv2.blur(img[y1:y1 + h, x1:x1 + w, :], (50, 50))
+            img[y1:y1 + h, x1:x1 + w, :] = cv2.blur(img[y1:y1 + h, x1:x1 + w, :], (75, 75))
+
+    return img
 
 
-# save image
-cv2.imwrite(os.path.join(output_dir, 'output.png'), img)
+args = argparse.ArgumentParser()
+
+args.add_argument('--mode', default='image')
+args.add_argument('--filePath', default='./data/testImg.png')
+
+args = args.parse_args()
+
+
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+
+
+# detect faces
+mp_face_detection = mp.solutions.face_detection
+
+with mp_face_detection.FaceDetection(model_selection=0, min_detection_confidence=0.5) as face_detection:
+
+    if args.mode in ['image']:
+        # read image
+        img = cv2.imread(args.filePath)
+
+        img = process_img(img, face_detection)    
+
+
+        # save image
+        cv2.imwrite(os.path.join(output_dir, 'output.png'), img)
+
+    elif args.mode in ['video']:
+
+        cap = cv2.VideoCapture(args.filePath)
+        ret, frame = cap.read()
+
+        output_video = cv2.VideoWriter(os.path.join(output_dir, 'output.mp4'),
+                                       cv2.VideoWriter_fourcc(*'MP4V'),
+                                       25,
+                                       (frame.shape[1], frame.shape[0]))
+
+        while ret:
+            frame = process_img(frame, face_detection)
+
+            output_video.write(frame)
+
+            ret, frame = cap.read()
+
+        cap.release()
+        output_video.release()
+
+    elif args.mode in ['webcam']:
+
+        cap = cv2.VideoCapture(0)
+        ret, frame = cap.read()
+
+        while ret:
+            frame = process_img(frame, face_detection)
+            cv2.imshow('frame', frame)
+            cv2.waitKey(25)
+
+            ret, frame = cap.read()
+            
+        cap.release()
